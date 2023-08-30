81c309c4765b9fe4f537388873609ee647c93de5
OfelyaMuradyan <138681012+OfelyaMuradyan@users.noreply.github.com>
2023-08-30 17:40:59 +0400

    pythonex3.py

--- pythonex3.py
+++ pythonex3.py
@@ -1,6 +1,8 @@
 x = int(input("Enter x: "))
 y = int(input("Enter y: "))
 
-res = abs(x - y) / (x + y)
-
-print(res)
+if (x + y) != 0:
+  res = abs(x - y) / (x + y)
+  print(res)
+else:
+  print("x + y can't be 0!")
